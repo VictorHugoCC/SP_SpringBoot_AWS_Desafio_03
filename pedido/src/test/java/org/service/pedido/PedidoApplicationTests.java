46f65e6fb3584e0ff0b20241fe65e99ae0286f31
Victor Hugo <victorcordeirojob@gmail.com>
2024-11-26 14:53:42 -0300

    Adicionando microserviços como parte do repositório principal

---
+++ pedido/src/test/java/org/service/pedido/PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.service.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
