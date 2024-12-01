e5e363ea2f1cc89b285fa4c37b9af2e62a622a25
Victor Hugo <victorcordeirojob@gmail.com>
2024-12-01 00:52:25 -0300

    :sparkles:feat: Adicionando swagger para cliente

--- cliente/src/main/java/org/service/cliente/controller/ClienteController.java
+++ cliente/src/main/java/org/service/cliente/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package org.service.cliente.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.service.cliente.dto.ClienteResponseDTO;
 import org.service.cliente.model.Cliente;
 import org.service.cliente.service.ClienteService;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
+@Tag(name = "Clientes", description = "Endpoints para gerenciar clientes")
 public class ClienteController {
 
     private final ClienteService clienteService;
@@ -18,13 +21,14 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    @Operation(summary = "Buscar todos os clientes", description = "Busca todos os clientes com seus pedidos associados")
     @GetMapping
     public ResponseEntity<List<ClienteResponseDTO>> getAllClientesComPedidos() {
         List<ClienteResponseDTO> clientesComPedidos = clienteService.findAllClientesComPedidos();
         return ResponseEntity.ok(clientesComPedidos);
     }
 
-
+    @Operation(summary = "Buscar cliente por ID", description = "Busca os detalhes de um cliente específico pelo ID")
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
         return clienteService.findClienteById(id)
@@ -32,11 +36,13 @@ public class ClienteController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Criar um novo cliente", description = "Cria um cliente e retorna os detalhes do cliente criado")
     @PostMapping
     public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
         return ResponseEntity.ok(clienteService.createCliente(cliente));
     }
 
+    @Operation(summary = "Atualizar cliente", description = "Atualiza os detalhes de um cliente específico pelo ID")
     @PutMapping("/{id}")
     public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
         try {
@@ -47,6 +53,7 @@ public class ClienteController {
         }
     }
 
+    @Operation(summary = "Deletar cliente", description = "Remove um cliente específico pelo ID")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
         try {
