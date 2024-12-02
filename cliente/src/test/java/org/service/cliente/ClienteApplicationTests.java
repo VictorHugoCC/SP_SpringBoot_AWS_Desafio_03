d10918344a86924bc9b839bec949d9232645a9f1
Victor Hugo <victorcordeirojob@gmail.com>
2024-12-02 00:44:24 -0300

    Refatorando codigo para actions

--- cliente/src/test/java/org/service/cliente/ClienteApplicationTests.java
+++ cliente/src/test/java/org/service/cliente/ClienteApplicationTests.java
@@ -2,10 +2,9 @@ package org.service.cliente;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class ClienteApplicationTests {
     @Test
     void contextLoads() {
